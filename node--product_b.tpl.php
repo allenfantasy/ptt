b0792a1c40b629ae581c76a149c0a8340102e08f
Allen Wu <allenwu1018@gmail.com>
2013-05-28 13:23:53 +0800

    refactor Prod-B's display

--- node--product_b.tpl.php
+++ node--product_b.tpl.php
@@ -1,55 +1,102 @@
+<?php
+  // produkt variables:
+  $produkt_object = $content['body']['#object'];
+  $lang = $produkt_object->language;
+
+  $custom = array();
+
+  $custom['title'] = $produkt_object->title;
+  $custom['body'] = $produkt_object->body['und'][0]['value'];
+
+  $custom['location'] = $produkt_object->field_location['und'][0]['value'];
+  $custom['price'] = $produkt_object->field_price['und'][0]['value']
+    . " " . $produkt_object->field_currency['und'][0]['taxonomy_term']->name
+    . " " . $produkt_object->field_price_unit['und'][0]['taxonomy_term']->name;
+
+  $custom['language'] = "";
+  if ($produkt_object->field_language_offer['und'][0]['value']) {
+    foreach($produkt_object->field_language_offer['und'] as $lang_offer) {
+      if($lang == 'en') { // EN
+        $custom['language'] .= $lang_offer['value'] . " ";
+      }
+      else { // DE
+        $language_value = $lang_offer['value'];
+        switch($language_value) {
+					case 'German':
+						$language_value = 'Deutsch';
+						break;
+					case 'English':
+						$language_value = 'Englisch';
+						break;
+					case 'Neutral':
+						$language_value = 'Neutral';
+						break;
+				}
+			  $custom['language'] .=  $language_value . " ";
+      }
+    }
+  }
+
+  $custom['info'] = $produkt_object->field_info['und'][0]['value'];
+  $custom['class'] = $produkt_object->field_class['und'][0]['value'];
+
+  $custom['code'] = $produkt_object->field_code['und'][0]['value'];
+?>
+
 <?php if ($content['body']['#object']->language=="en") {?>
 	<div id="node-<?php print $node->nid; ?>" class="prodoct-page <?php print $classes; ?>"<?php print $attributes; ?>>
 		<div class="page-header">
 			<?php print render($content['field_image']); ?>
 		</div>
 		<div class="title">
-			<h3><?php echo $content['body']['#object']->title; ?></h3>
+			<h3><?php echo $custom['title']; ?></h3>
 		</div>
 		<div class="content">
-			<?php echo $content['body']['#object']->body['und'][0]['value']; ?>
+			<?php echo $custom['body']; ?>
 		</div>
+
 		<div class="info1">
-			<b class="label">Location :</b> <?php echo $content['body']['#object']->field_location['und'][0]['value']; ?>
+    <?php if($custom['location']) { ?>
+			<b class="label">Location :</b> <?php echo $custom['location']; ?>
+    <?php } ?>
 		</div>
+
 		<div class="info2">
-			<b class="label">Price :</b> <?php echo $content['body']['#object']->field_price['und'][0]['value']; ?> <?php echo $content['body']['#object']->field_currency['und'][0]['taxonomy_term']->name; ?> <?php echo $content['body']['#object']->field_price_unit['und'][0]['taxonomy_term']->name; ?>
+    <?php if($custom['price']) { ?>
+			<b class="label">Price :</b> <?php echo $custom['price']; ?>
+    <?php } ?>
 		</div>
+
 		<div class="info3">
-			<b class="label">Language :</b>
-			<?php
-				for($i = 0; $i < count($content['body']['#object']->field_language_offer['und']); $i++){
-					echo $content['body']['#object']->field_language_offer['und'][$i]['value'];
-					if(($i+1)!=count($content['body']['#object']->field_language_offer['und'])){
-					echo ' , ';
-					}
-				}
-			?>
+    <?php if($custom['language']) { ?>
+			<b class="label">Language :</b> <?php echo $custom['language']; ?>
+    <?php } ?>
 		</div>
+
 		<div class="info4">
-			<?php if($content['body']['#object']->field_info['und'][0]['value']) {?>
-				<b class="label">General Information</b>
-				<br/>
-				<?php echo $content['body']['#object']->field_info['und'][0]['value']; ?>
+			<?php if($custom['info']) { ?>
+				<b class="label">General Information</b><br/><?php echo $custom['info']; ?>
 			<?php } ?>
 		</div>
+
 		<div class="info5">
-			<?php if($content['body']['#object']->field_class['und'][0]['value']) {?>
-				<b class="label">Class : </b><?php echo $content['body']['#object']->field_class['und'][0]['value']; ?>
+      <?php if($custom['class']) { ?>
+        <b class="label">Class : </b><?php echo $custom['class']; ?>
 			<?php } ?>
 		</div>
+
 		<?php //print_r($content); ?>
 		<div class="to-buy">
 			<form action="/en/basket?option=add" method="post">
-				<input type="hidden" name="code" value="<?php echo $content['body']['#object']->field_code['und'][0]['value'];?>" />
-				<input type="hidden" name="price" value="<?php echo $content['body']['#object']->field_price['und'][0]['value']; ?> <?php echo $content['body']['#object']->field_currency['und'][0]['taxonomy_term']->name; ?> <?php echo $content['body']['#object']->field_price_unit['und'][0]['taxonomy_term']->name; ?>"/>
-				<input type="hidden" name="title" value="<?php echo $content['body']['#object']->title; ?>"/>
+        <input type="hidden" name="code" value="<?php echo $custom['code']; ?>"/>
+        <input type="hidden" name="price" value="<?php echo $custom['price']; ?>"/>
+				<input type="hidden" name="title" value="<?php echo $custom['title']; ?>"/>
 				<input type="hidden" name="url" value="<?php echo $variables['node_url']; ?>" />
 				<?php
-					$code = $content['body']['#object']->field_code['und'][0]['value'];
+          // Risk: two products may have same code, which would cause missing order.
 					$check = false;
 					for($k=0;$k<count($_SESSION['basket']);$k++){
-						if($code==$_SESSION['basket'][$k]['code']){
+						if($custom['code']==$_SESSION['basket'][$k]['code']){
 						  	$check = true;
 						}
 					}
@@ -69,65 +116,54 @@
 			<?php print render($content['field_image']); ?>
 		</div>
 		<div class="title">
-			<h3><?php echo $content['body']['#object']->title; ?></h3>
+			<h3><?php echo $custom['title']; ?></h3>
 		</div>
 		<div class="content">
-			<?php echo $content['body']['#object']->body['und'][0]['value']; ?>
+			<?php echo $custom['body']; ?>
 		</div>
-		<div class="info1">
-			<b class="label">Ort :</b> <?php echo $content['body']['#object']->field_location['und'][0]['value']; ?>
-		</div>
-		<div class="info2">
-			<b class="label">Preis :</b> <?php echo $content['body']['#object']->field_price['und'][0]['value']; ?> <?php echo $content['body']['#object']->field_currency['und'][0]['taxonomy_term']->name; ?> <?php echo $content['body']['#object']->field_price_unit['und'][0]['taxonomy_term']->name; ?>
-		</div>
-		<div class="info3">
-			<b class="label">Sprache :</b>
-			<?php
-				//for($i = 0; $i < count($content['body']['#object']->field_language_offer['und']); $i++){
-					$language_value =  $content['body']['#object']->field_language_offer['und'][0]['value'];
-					switch($language_value) {
-					case 'German':
-						$language_value = 'Deutsch';
-						break;
-					case 'English':
-						$language_value = 'Englisch';
-						break;
-					case 'Neutral':
-						$language_value = 'Neutral';
-						break;
-					}
-					echo $language_value;
 
-				//	if(($i+1)!=count($content['body']['#object']->field_language_offer['und'])){
-				//	echo ' , ';
-				//	}
-				//}
-			?>
+		<div class="info1">
+    <?php if ($custom['location']) { ?>
+			<b class="label">Ort :</b> <?php echo $custom['location']; ?>
+    <?php } ?>
 		</div>
+
+		<div class="info2">
+    <?php if ($custom['price']) { ?>
+      <b class="label">Preis :</b> <?php echo $custom['price']; ?>
+    <?php } ?>
+		</div>
+
+		<div class="info3">
+      <?php if($custom['language']) { ?>
+			<b class="label">Sprache :</b> <?php echo $custom['language']; ?>
+      <?php } ?>
+		</div>
+
 		<div class="info4">
-			<?php if($content['body']['#object']->field_info['und'][0]['value']) { ?>
-				<b class="label">Hinweise</b>
-				<br/>
-				<?php echo $content['body']['#object']->field_info['und'][0]['value']; ?>
+			<?php if($custom['info']) { ?>
+				<b class="label">Hinweise</b> <br/>
+				<?php echo $custom['info']; ?>
 			<?php } ?>
 		</div>
+
 		<div class="info5">
-			<?php if($content['body']['#object']->field_class['und'][0]['value']) { ?>
-				<b class="label">Klasse :</b> <?php echo $content['body']['#object']->field_class['und'][0]['value']; ?>
+      <?php if($custom['class']) { ?>
+        <b class="label">Klasse :</b> <?php echo $custom['class']; ?>
 			<?php } ?>
 		</div>
 
 		<div class="to-buy">
 			<form action="/de/korb?option=add" method="post">
-				<input type="hidden" name="code" value="<?php echo $content['body']['#object']->field_code['und'][0]['value'];?>" />
-				<input type="hidden" name="price" value="<?php echo $content['body']['#object']->field_price['und'][0]['value']; ?> <?php echo $content['body']['#object']->field_currency['und'][0]['taxonomy_term']->name; ?> <?php echo $content['body']['#object']->field_price_unit['und'][0]['taxonomy_term']->name; ?>"/>
-				<input type="hidden" name="title" value="<?php echo $content['body']['#object']->title; ?>"/>
+				<input type="hidden" name="code" value="<?php echo $custom['code']; ?>" />
+        <input type="hidden" name="price" value="<?php echo $custom['price']; ?>"/>
+				<input type="hidden" name="title" value="<?php echo $custom['title']; ?>"/>
 				<input type="hidden" name="url" value="<?php echo $variables['node_url']; ?>" />
 				<?php
-					$code = $content['body']['#object']->field_code['und'][0]['value'];
+          // Risk: two products may have same code, which would cause missing order.
 					$check = false;
 					for($k=0;$k<count($_SESSION['basket']);$k++){
-						if($code==$_SESSION['basket'][$k]['code']){
+						if($custom['code']==$_SESSION['basket'][$k]['code']){
 						  	$check = true;
 						}
 					}
